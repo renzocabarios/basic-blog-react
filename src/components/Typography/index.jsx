93725f603d15ef097edb6c70f7d58568673110aa
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-04-25 03:19:14 +0800

    Update Typography

--- src/components/Typography/index.jsx
+++ src/components/Typography/index.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import Typography from "@mui/material/Typography";
 
 export default function (props) {
-  const { component, variant, color, align, noWrap, sx, title } = props;
+  const {
+    component,
+    variant,
+    color,
+    align,
+    noWrap,
+    sx,
+    title,
+    paragraph = false,
+    gutterBottom = false,
+  } = props;
   return (
     <>
       <Typography
@@ -12,6 +22,8 @@ export default function (props) {
         align={align}
         noWrap={noWrap}
         sx={sx}
+        paragraph={paragraph}
+        gutterBottom={gutterBottom}
       >
         {title}
       </Typography>
