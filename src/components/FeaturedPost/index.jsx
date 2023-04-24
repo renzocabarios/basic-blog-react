f065402ad7488fd9fdc68191071ac43900b1ad87
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-04-25 05:09:06 +0800

    Update FeaturedPost

--- src/components/FeaturedPost/index.jsx
+++ src/components/FeaturedPost/index.jsx
@@ -8,10 +8,11 @@ import {
   CardMedia,
 } from "@mui/material";
 import { useState } from "react";
-
+import { useNavigate } from "react-router-dom";
 export default function (props) {
   const [more, setmore] = useState(false);
-  const { image, date, title, description, imageLabel } = props;
+  const navigate = useNavigate();
+  const { id, image, date, title, description, imageLabel } = props;
 
   const descriptionArray = description.split("");
   descriptionArray.splice(90);
@@ -19,7 +20,12 @@ export default function (props) {
   return (
     <Grid item xs={12} md={6}>
       <CardActionArea component="a">
-        <Card sx={{ display: "flex" }}>
+        <Card
+          sx={{ display: "flex" }}
+          onClick={() => {
+            navigate(`/${id}`);
+          }}
+        >
           <CardContent sx={{ flex: 1 }}>
             <Typography component="h2" variant="h5" title={title} />
             <Typography
